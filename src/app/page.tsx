25c0fd4060faff0518b55f3a32fb80ec1935cd6b
Gabriel <ar.gabrielss@gmail.com>
2025-07-01 20:38:38 -0300

    Create footer and add to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from './_components/Header/header'
 import { Hero } from './_components/Hero/hero'
+import Footer from './_components/Footer/footer'
 
 export default function Home() {
   return(
     <div>
       <Header />
       <Hero />
+      <Footer />
     </div>
   );
 }
